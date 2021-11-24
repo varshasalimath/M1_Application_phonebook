1a8f9adb958bf554cbc974be868c0998b1aba7d0
varshasalimath <89127077+varshasalimath@users.noreply.github.com>
2021-11-24 22:52:18 +0530

    Add files via upload

---
+++ 3_Implementation/phonebook.h
@@ -0,0 +1,62 @@
+/**
+ * @file phonebook.h
+ * @author Varsha (varsha.salimath@ltts.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief structure for phonebook
+ * 
+ */
+
+/**
+ * @brief  return type for function for unit testing
+ * 
+ */
+ 
+ typedef enum test_values{
+    pass = 1;
+    fail = 0;
+    }test_values;
+
+/**
+ * @brief function to find a operation phonebook_operations
+ * 'H','h'
+ * @return test_values
+ * 
+ */
+
+void insert_to_phonebook_linkedlist()
+/**
+ * @brief function to perform insertion
+ * switch(phonebook_oprn)
+ * @return insert_to_phonebook(); 
+ */
+ 
+ void list_phonebook_linkedlist()
+
+ /**
+ * @brief function to perform listing 
+ * switch(phonebook_oprn)
+ * @return list_entries_phonebook();
+ */
+
+
+void delete_from_phonebook_linkedlist()
+
+/**
+ * @brief function to perform deletion
+ * switch(phonebook_oprn)
+ * @return delete_from_phonebook();
+ */
+
+#endif
